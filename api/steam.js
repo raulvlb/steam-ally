cedfdb96cab2706ca0da2eaed7bf0047421a90bd
raulvlb <raulvlb1942000@gmail.com>
2026-01-30 17:00:58 -0300

    Fix Vercel API routing configuration

--- api/steam.js
+++ api/steam.js
@@ -22,7 +22,7 @@ async function fetchSteamAPI(url) {
 }
 
 // Main handler
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   // Enable CORS
   res.setHeader('Access-Control-Allow-Credentials', 'true');
   res.setHeader('Access-Control-Allow-Origin', '*');
